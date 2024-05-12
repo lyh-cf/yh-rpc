0daba04842af5ff2cf9a36f08eb3d5c07f7b1d64
lyh-cf <1470511515@qq.com>
2024-05-12 20:05:46 +0800

    feature：开发rpc-easy模块的核心（请求处理器）

--- yh-rpc-easy/src/main/java/com/lyh/rpc/server/VertxHttpServer.java
+++ yh-rpc-easy/src/main/java/com/lyh/rpc/server/VertxHttpServer.java
@@ -9,8 +9,16 @@ import io.vertx.core.Vertx;
  *@version 1.0
  *@create 2024/5/12 16:44
  */
+/**
+ * Vertx HTTP 服务器
+ */
 public class VertxHttpServer implements HttpServer {
 
+    /**
+     * 启动服务器
+     *
+     * @param port
+     */
     public void doStart(int port) {
         // 创建 Vert.x 实例
         Vertx vertx = Vertx.vertx();
@@ -19,15 +27,7 @@ public class VertxHttpServer implements HttpServer {
         io.vertx.core.http.HttpServer server = vertx.createHttpServer();
 
         // 监听端口并处理请求
-        server.requestHandler(request -> {
-            // 处理 HTTP 请求
-            System.out.println("Received request: " + request.method() + " " + request.uri());
-
-            // 发送 HTTP 响应
-            request.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello from Vert.x HTTP server!");
-        });
+        server.requestHandler(new HttpServerHandler());
 
         // 启动 HTTP 服务器并监听指定端口
         server.listen(port, result -> {
